77a591f2add90bcb17014a0209d00e96eb9fb6d0
atan0002 <natalia.wisniewsqa@gmail.com>
2023-03-14 22:51:31 +0100

    initial

---
+++ TSwR_student/controllers/feedback_linearization_controller.py
@@ -0,0 +1,15 @@
+import numpy as np
+from models.manipulator_model import ManiuplatorModel
+from .controller import Controller
+
+
+class FeedbackLinearizationController(Controller):
+    def __init__(self, Tp):
+        self.model = ManiuplatorModel(Tp)
+
+    def calculate_control(self, x, q_r, q_r_dot, q_r_ddot):
+        """
+        Please implement the feedback linearization using self.model (which you have to implement also),
+        robot state x and desired control v.
+        """
+        return NotImplementedError()
---
+++ TSwR_student/models/manipulator_model.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+class ManiuplatorModel:
+    def __init__(self, Tp):
+        self.Tp = Tp
+        self.l1 = 0.5
+        self.r1 = 0.01
+        self.m1 = 1.
+        self.l2 = 0.5
+        self.r2 = 0.01
+        self.m2 = 1.
+        self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
+        self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2)
+        self.m3 = 0.0
+        self.r3 = 0.01
+        self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
+
+    def M(self, x):
+        """
+        Please implement the calculation of the mass matrix, according to the model derived in the exercise
+        (2DoF planar manipulator with the object at the tip)
+        """
+        q1, q2, q1_dot, q2_dot = x
+        return NotImplementedError()
+
+    def C(self, x):
+        """
+        Please implement the calculation of the Coriolis and centrifugal forces matrix, according to the model derived
+        in the exercise (2DoF planar manipulator with the object at the tip)
+        """
+        q1, q2, q1_dot, q2_dot = x
+        return NotImplementedError()
