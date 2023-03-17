9c00ae744a075e47e9ad17690758586451d18d89
atan0002 <natalia.wisniewsqa@gmail.com>
2023-03-17 13:40:33 +0100

    works

--- TSwR_student/models/manipulator_model.py
+++ TSwR_student/models/manipulator_model.py
@@ -5,21 +5,25 @@ class ManiuplatorModel:
     def __init__(self, Tp):
         self.Tp = Tp
         self.l1 = 0.5
-        self.r1 = 0.01
-        self.m1 = 1.
-        self.l2 = 0.5
-        self.r2 = 0.01
-        self.m2 = 1.
-        self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
-        self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2)
-        self.m3 = 0.0
-        self.r3 = 0.01
-        self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
+        self.r1 = 0.04
+        self.m1 = 3.
+        self.l2 = 0.4
+        self.r2 = 0.04
+        self.m2 = 2.4
+       
+        self.m3 = 0.2
+        self.r3 = 0.05
+      
         self.d1=self.l1/2
         self.d2=self.l2/2
-        self.alpha=self.m1*self.d1*self.d1+self.I_1+self.m2*(self.l1*self.l1+self.d2*self.d2) + self.I_2
-        self.beta=self.m2*self.l1*self.d2
-        self.gamma=self.m2*self.d2*self.d2+self.I_2
+        self.d2_new=self.d2#(self.d2*self.m2+((self.l2+self.r3)*self.m3))/(self.m2+self.m3)
+        self.m_new=self.m2
+        self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
+        self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2) #+ self.m2*(self.d2_new-self.d2)**2
+        self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2 #+self.m3*(self.l2-self.d2_new)**2
+        self.alpha=self.m1*self.d1**2+self.I_1+self.m2*(self.l1**2+self.d2**2) + self.I_2 + self.m3*(self.l1**2+(self.l2**2))+self.I_3
+        self.beta=self.m2*self.l1*self.d2 + self.m3*((self.l2)*self.l1)
+        self.gamma=self.m2*self.d2**2 +self.I_2+self.m3*(self.l2)**2+self.I_3
         
 
     def M(self, x):
@@ -29,6 +33,7 @@ class ManiuplatorModel:
         """
         q1, q2, q1_dot, q2_dot = x
 
+        #Macierz  dla manipulatora bez m3
         M = np.array([[self.alpha+2*self.beta*np.cos(q2), self.gamma+self.beta*np.cos(q2) ],[self.gamma+self.beta*np.cos(q2), self.gamma]])
         
 
@@ -42,5 +47,9 @@ class ManiuplatorModel:
         """
         q1, q2, q1_dot, q2_dot = x
 
+        #macierz C dla manipulatora bez m3
+
         C=np.array([[-self.beta*np.sin(q2)*q2_dot,-self.beta*np.sin(q2)*(q1_dot+q2_dot)],[self.beta*np.sin(q2)*q1_dot,0]])
+        
+        
         return C
