72854319305e900150607270504f6a5fc423ca63
atan0002 <natalia.wisniewsqa@gmail.com>
2023-04-11 21:19:23 +0200

    adrcflc eso

--- TSwR_student/controllers/adrc_flc_controller.py
+++ TSwR_student/controllers/adrc_flc_controller.py
@@ -14,22 +14,67 @@ class ADRFLController(Controller):
         self.model = None
         self.Kp = Kp
         self.Kd = Kd
-        self.I=np.array([[self.manipulator.I_1,self.manipulator.I_3],[0,self.manipulator.I_2]])
+        self.I=np.ones(2,2)#np.array([[self.manipulator.I_1,self.manipulator.I_3],[0,self.manipulator.I_2]])
         self.L = None
-        W = np.array([self.I,np.zeros(2,4)])
-        A = None#np.array([[np.zeros(2,2),self.I,np.zeros(2,2)],[np.zeros(2,2),-self.manipulator.M]])
-        B = None
-        self.eso = ESO(A, B, W, self.L, q0, Tp)
+        self.W = np.array([self.I,np.zeros(2,4)]).reshape(6,2)
+        self.A = None#np.array([[np.zeros(2,2),self.I,np.zeros(2,2)],[np.zeros(2,2),-self.manipulator.M]])
+        self.B = None
+        self.eso = ESO(self.A, self.B, self.W, self.L, q0, Tp)
+        self.omega=p
         self.update_params(q0[:2], q0[2:])
+        self.Tp=Tp
+        self.x_est_n1=np.zeros(6,1)
+        self.u=np.zeros(2,1)
+        self.model_est=ManiuplatorModel(Tp)
+        self.omegac=30
+        self.ksi=1
+        self.Kp = self.omegac**2
+        self.Kd = 2*self.ksi*self.omegac
+
 
     def update_params(self, q, q_dot):
         ### TODO Implement procedure to set eso.A and eso.B
         x=np.array([q,q_dot])
         M_inv=np.linalg.inv(self.manipulator.M(x))
+        N=-M_inv@self.manipulator.C(x)
+    
+        self.l1= N[0,0]/2+N[1,1]/2+3*self.omega
+        self.l3=(N[0,0]**3*self.I[0,1] - N[0,0]**2*N[0,1]*self.I[0,0] + 6*N[0,0]**2*self.I,[0,1]*self.omega + 
+                 2*N[0,0]*N[0,1]*N[1,0]*self.I[0,1] - N[0,0]*N[0,1]*N[1,1]*self.I[0,0] - 6*N[0,0]*N[0,1]*self.I[0,0]*self.omega + 
+                 15*N[0,0]*self.I[0,1]*self.omega**2 - N[0,1]**2*N[1,0]*self.I[0,0] + N[0,1]*N[1,0]*N[1,1]*self.I[0,1] + 6*N[0,1]*N[1,0]*self.I[0,1]*self.omega - 
+                 N[0,1]*N[1,1]**2*self.I[0,0] - 6*N[0,1]*N[1,1]*self.I[0,0]*self.omega - 15*N[0,1]*self.I[0,0]*self.omega**2 + 20*self.I[0,1]*self.omega**3)/(2*N[0,0]*self.I[0,0]*self.I[0,1] - 2*N[0,1]*self.I[0,0]**2 + 2*N[1,0]*self.I[0,1]**2 - 2*N[1,1]*self.I[0,0]*self.I[0,1])
+        
+        self.l4=(N[0,0]**2*N[1,0]*self.I[0,1] - N[0,0]*N[0,1]*N[1,0]*self.I[0,0] + N[0,0]*N[1,0]*N[1,1]*self.I[0,1] + 6*N[0,0]*N[1,0]*self.I01*self.omega + N[0,1]*N[1,0]**2*self.I[0,1] - 
+                 2*N[0,1]*N[1,0]*N[1,1]*self.I[0,0] - 6*N[0,1]*N[1,0]*self.I[0,0]*self.omega + N[1,0]*N[1,1]**2*self.I[0,1] + 
+                 6*N[1,0]*N[1,1]*self.I[0,1]*self.omega + 15*N[1,0]*self.I[0,1]*self.omega**2 - N[1,1]**3*self.I[0,0] - 6*N[1,1]**2*self.I[0,0]*self.omega -
+                   15*N[1,1]*self.I[0,0]*self.omega**2 - 20*self.I[0,0]*self.omega**3)/(2*N[0,0]*self.I[0,0]*self.I[0,1] - 2*N[0,1]*self.I[0,0]**2 + 2*N[1,0]*self.I[0,1]**2 - 2*N[1,1]*self.I[0,0]*self.I[0,1])
+        
+       
+        self.L=np.array([self.l1,self.l1],[0,0],[self.l3,self.l3],[self.l4,self.l4],[0,0],[0,0])
+
         self.eso.A = np.array([[np.zeros(2,2),self.I,np.zeros(2,2)],[np.zeros(2,2),-M_inv@self.manipulator.C(x),self.I],
                                [np.zeros(2,2),np.zeros(2,2),np.zeros(2,2)]])
         self.eso.B = self.array([np.zeros(2,2),M_inv,np.zeros(2,2)])
 
     def calculate_control(self, x, q_d, q_d_dot, q_d_ddot):
-        ### TODO implement centralized ADRFLC
-        return NotImplementedError
+
+        q=x[:2]
+        q_dot=x[2:]
+        
+        self.Ad=np.eye(6)+self.Tp*self.A
+        self.Bd=self.Tp*self.B
+        self.Od=self.Tp*self.L
+        
+
+
+        x_est=self.Ad@self.x_est_n1+self.Bd*self.u+self.Od*(q-self.W@self.x_est_n1)
+
+        v=self.Kp*(q_d.reshape(2,1)-q)+ self.Kd*(q_d_dot.reshape(2,1)-q_dot)+q_d_ddot
+
+
+        u=self.model_est.M(x_est)@(v-x_est[4:])+self.model_est.C(x_est)@x_est[2:4]
+
+
+        self.update_params(x_est[:2],x_est[2:4])
+        
+        return u
--- TSwR_student/utils/calcL.py
+++ TSwR_student/utils/calcL.py
@@ -4,60 +4,72 @@ from sympy import *
 
 class CalculateL:
     def __init__(self):
-        
-       pass
+        self.lam=symbols('lam')
+        self.omega=symbols('self.omega')
+        self.bandw=(self.lam+self.omega)
+        # sesolution=Pow(band,7)
+        self.I00=symbols('self.I00')
+        self.I01=symbols('self.I01')
+        self.I10=symbols('self.I10')
+        self.I11=symbols('self.I11')
+        self.N00=symbols('N00')
+        self.N01=symbols('N01')
+        self.N10=symbols('N10')
+        self.N11=symbols('N11')
+        self.l1_1=symbols('l1_1')
+        self.l1_2=symbols('l1_2')
+        self.l1_3=symbols('l1_3')
+        self.l1_4=symbols('l1_4')
+        self.l1_5=symbols('l1_5')
+        self.l1_6=symbols('l1_6')
+        # l2_1=symbols('l2_1')
+        # l2_2=symbols('l2_2')
+        # l2_3=symbols('l2_3')
+        # l2_4=symbols('l2_4')
+        # l2_5=symbols('l2_5')
+        # l2_6=symbols('l2_6')
+
+    
 
 
     def solveExpr(self):
-        lam=symbols('lam')
-        omega=symbols('omega')
-        band=(lam+omega)
-        solution=Pow(band,7)
-        I00=symbols('I00')
-        I01=symbols('I01')
-        I10=symbols('I10')
-        I11=symbols('I11')
-        N00=symbols('N00')
-        N01=symbols('N01')
-        N10=symbols('N10')
-        N11=symbols('N11')
-        l1_1=symbols('l1_1')
-        l1_2=symbols('l1_2')
-        l1_3=symbols('l1_3')
-        l1_4=symbols('l1_4')
-        l1_5=symbols('l1_5')
-        l1_6=symbols('l1_6')
-        l2_1=symbols('l2_1')
-        l2_2=symbols('l2_2')
-        l2_3=symbols('l2_3')
-        l2_4=symbols('l2_4')
-        l2_5=symbols('l2_5')
-        l2_6=symbols('l2_6')
-
+       
 
         
 
-        A=Matrix([[0,0,I00,I01,0,0],[0,0,I10,I11,0,0],
-                 [0,0,N00,N01,0,0],[0,0,N10,N11,0,0],
+        A=Matrix([[0,0,self.I00,self.I01,0,0],[0,0,self.I10,self.I11,0,0],
+                 [0,0,self.N00,self.N01,0,0],[0,0,self.N10,self.N11,0,0],
                  [0,0,0,0,0,0],
                  [0,0,0,0,0,0]])
         
-        L=Matrix([[l1_1,l2_1],[l1_2,l2_2],[l1_3,l2_3],[l1_4,l2_4],[l1_5,l2_5],[l1_6,l2_6]])
+        L=Matrix([[self.l1_1,self.l1_1],[self.l1_2,self.l1_2],[self.l1_3,self.l1_3],[self.l1_4,self.l1_4],[self.l1_5,self.l1_5],[self.l1_6,self.l1_6]])
         C=Matrix([[1,0,0,0,0,0],[1,0,0,0,0,0]])   
         H=A-L*C
-        I=eye(6)*lam
+        I=eye(6)*self.lam
         Wd=I-H
+        print(f'Wd: {Wd}')
         Wd=Wd.det()
 
-        return Wd.as_poly(lam).coeffs()#solution.as_poly()
+        return Wd.as_poly(self.lam).coeffs()#solution.as_poly()
     
-    
+    def band(self):
+       
+        band=Pow((self.lam+self.omega),6)
+        return band.as_poly(self.lam).coeffs()
 
 
     def main(self):
+       
         sol=self.solveExpr()
-        print(sol)
-        # print((sol))
+        # print('sol:',sol)
+        sol2=self.band()
+        # print(f'band: {sol2}')
+        eq=[sol2[0]-sol[0],sol2[1]-sol[1],sol2[2]-sol[2],sol2[3]-sol[3],sol2[4],sol2[5],sol2[6]]
+        # print(eq)
+
+        solution=solve(eq,[self.l1_1,self.l1_2,self.l1_3,self.l1_4,self.l1_5,self.l1_6],dict=True)
+        print('l1 ',solution)
+
 
 
 if __name__ == "__main__":
