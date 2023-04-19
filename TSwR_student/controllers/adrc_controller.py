155cc344c7758f0fa80b687855ddfc8697597b22
atan0002 <natalia.wisniewsqa@gmail.com>
2023-04-19 20:10:54 +0200

    adrc fixes

--- TSwR_student/controllers/adrc_controller.py
+++ TSwR_student/controllers/adrc_controller.py
@@ -14,12 +14,12 @@ class ADRController(Controller):
 
     def calculate_control(self, x, q_d, q_d_dot, q_d_ddot):
         u = []
-        M=self.manModel.M(x)
-        M_inv=np.linalg.inv(M)
-        b=[M_inv[0,0],M_inv[1,1]]
+        # M=self.manModel.M(x)
+        # M_inv=np.linalg.inv(M)
+        # b=[M_inv[0,0],M_inv[1,1]]
 
         for i, controller in enumerate(self.joint_controllers):
-            controller.set_b(b[i])
+            # controller.set_b(b[i])
             print(f'Controller {i}, b:{controller.b}')
             u.append(controller.calculate_control([x[i], x[i+2]], q_d[i], q_d_dot[i], q_d_ddot[i]))
         u = np.array(u)[:, np.newaxis]
